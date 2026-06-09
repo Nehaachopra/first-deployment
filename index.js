a0b853a12a14d728a3e10f190ba399150de66d81
Neha Chopra <chopraneha211@gmail.com>
2026-06-09 15:07:25 +0530

    testing changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello! How have you been doing?");
+  res.send("Hello world");
 });
 
 app.get("/health", (req, res) => {
